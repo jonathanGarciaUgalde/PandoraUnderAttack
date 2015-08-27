ab3cf4c3ec471de11c5003f489526f8fb148e225
jonathan <jonagaru1@hotmail.com>
2015-08-26 23:43:35 -0600

    prueba

--- src/cr/ac/itcr/Main.java
+++ src/cr/ac/itcr/Main.java
@@ -2,7 +2,7 @@ package cr.ac.itcr;
 
 public class Main {
 
-    public static final String HOLA_MUNDO = "wea  mundo";
+    public static final String HOLA_MUNDO = "weahfhfh  mundo";
 
     public static void main(String[] args) {
 	// write your code here h
