a7d01407d9d74545360acf0d075bd075c23bf123
jonathan <jonagaru1@hotmail.com>
2015-08-26 23:08:00 -0600

    maven

--- src/cr/ac/itcr/Main.java
+++ src/cr/ac/itcr/Main.java
@@ -2,7 +2,15 @@ package cr.ac.itcr;
 
 public class Main {
 
+    public static final String HOLA_MUNDO = "hola  mundo";
+
     public static void main(String[] args) {
-	// write your code here
+	// write your code here h
+
+        System.out.println();
+        for (int hhfnhtnbfh = 0; hhfnhtnbfh < 567576; hhfnhtnbfh++) {
+
+        }
+        System.out.println(HOLA_MUNDO);
     }
 }
