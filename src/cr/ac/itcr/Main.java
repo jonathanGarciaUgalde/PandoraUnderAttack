4cae0661da13dc10910f58ba0c39a288fd5a9b11
Gustavo Hernandez <ghernandezgranera16@gmail.com>
2015-09-08 13:29:51 -0600

    Change

--- src/cr/ac/itcr/Main.java
+++ src/cr/ac/itcr/Main.java
@@ -2,18 +2,9 @@ package cr.ac.itcr;
 
 public class Main {
 
-    public static final String HOLA_MUNDO = "Steven!!";
-
     public static void main(String[] args) {
-	// write your code here h
 
-
-        for (int hhfnhtnbfh = 0; hhfnhtnbfh < 300; hhfnhtnbfh++) {
-            System.out.println("mae esa vara ya esta  me lleva puta  llisto");
-            System.out.println("hola mundo");
-
-
-        }
-        System.out.println(HOLA_MUNDO);
+		}
+    	
     }
-}
+
