4fa1442e72fc519f2a9fc898033071f5335ba138
jonathan <jonagaru1@hotmail.com>
2015-09-16 23:31:54 -0600

    esta si?

--- src/cr/ac/itcr/server/MainServer.java
+++ src/cr/ac/itcr/server/MainServer.java
@@ -6,10 +6,9 @@ public class MainServer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Server servidor= new Server();
-		System.out.println(servidor.insertar());
 		// TODO Auto-generated method stub
 
-	}
+		Server servidor= new Server();
+		System.out.println(servidor.insertar());}
 
 }
