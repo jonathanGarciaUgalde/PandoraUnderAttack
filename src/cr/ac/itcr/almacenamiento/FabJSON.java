0f783ff665e3700b9170b7aeabcbc24fa54a0f63
Gustavo Hernandez <ghernandezgranera16@gmail.com>
2015-09-20 16:55:14 -0600

    .

--- src/cr/ac/itcr/almacenamiento/FabJSON.java
+++ src/cr/ac/itcr/almacenamiento/FabJSON.java
@@ -1,6 +1,7 @@
 package cr.ac.itcr.almacenamiento;
 
-import org.json.simple.JSONObject;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FabJSON {
 
@@ -8,23 +9,32 @@ public class FabJSON {
 	
 	//Método para agregar un elemento al JSON
 	public void addElement(String titulo, String elemento){
-		original.put(titulo, elemento);
+		try {
+			original.put(titulo, elemento);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	// Método para agregar un JSON a una lista
 	public ListaDoble addJsonList(){
 		ListaDoble lista = new ListaDoble();
-		lista.agregarInicio(original.toJSONString());
+		lista.agregarInicio(original.toString());
 		return lista;
 	}
 	
 	//Método para agregar un elemento tipo lista a una lista
 	public void addListJson(String cabecera,ListaDoble listajson){
 		String datos=listajson.mostrarInicioFin();
-		original.put(cabecera, datos);
-		System.out.print(datos);
-		System.out.println();
-		System.out.println(original);
+		try {
+			original.put(cabecera, datos);
+			System.out.print(datos);
+			System.out.println();
+			System.out.println(original);
+		} catch (JSONException e) {
+
+			e.printStackTrace();
+		}
 	}
 
 	
